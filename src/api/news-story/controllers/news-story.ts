d64cd1bd6e312b5eda072110c56b72c36cbc4342
John Eatmon <jeatmon@getprosource.com>
2023-02-14 13:32:03 -0800

    feat: add content collections
    Case studies
    Blog posts
    Company news
    Guides and ebooks
    Terms and conditions
    Videos
    Webinars

---
+++ src/api/news-story/controllers/news-story.ts
@@ -0,0 +1,7 @@
+/**
+ * news-story controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::news-story.news-story');
---
+++ src/api/news-story/services/news-story.ts
@@ -0,0 +1,7 @@
+/**
+ * news-story service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::news-story.news-story');
